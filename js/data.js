0e86727de08124bcdc43162699bbcc7b7ccb804e
szabi1112 <banki.szabolcs@gmail.com>
2022-09-11 19:18:50 +0200

    Félúton

---
+++ js/data.js
@@ -0,0 +1,51 @@
+const getData = async () => {
+  try {
+    const result = await fetch('./json/got.json').then((data) => data.json());
+    return result;
+  } catch (error) {
+    console.error(`Valami hiba van a adatok lekérésekor: ${error}`);
+    return '';
+  }
+};
+
+const filterData = async () => {
+  try {
+    const arr = await getData().then((data) => data);
+    const filteredArr = arr.filter((item) => item.dead !== true).slice(0, -1);
+    return filteredArr;
+  } catch (error) {
+    console.error(`Valami hiba van az adatok szűrésekor: ${error}`);
+    return '';
+  }
+};
+
+const sortArr = async () => {
+  try {
+    const arr = await filterData().then((data) => data);
+    const sortedArr = arr.sort((a, b) => a.name.split(' ').slice(-1)[0].localeCompare(b.name.split(' ').slice(-1)[0], 'hu'));
+
+    return sortedArr;
+  } catch (error) {
+    console.error(`Valami hiba van az adatok rendezésekor: ${error}`);
+    return '';
+  }
+};
+
+const findPerson = async (name) => {
+  try {
+    const result = await sortArr();
+    const person = result.find((item) => item.name.toLowerCase() === name);
+    return person;
+  } catch (error) {
+    console.error('Nem találtunk a keresésnek megfelelő személyt!');
+    return '';
+  }
+};
+
+const actualPerson = await findPerson('hodor');
+console.log(actualPerson);
+
+export {
+  sortArr,
+  findPerson,
+};
--- js/main.js
+++ js/main.js
@@ -1,36 +1,86 @@
-const getData = async () => {
-  try {
-    const result = await fetch('./json/got.json').then((data) => data.json());
-    return result;
-  } catch (error) {
-    console.error(`Valami hiba van a adatok lekérésekor: ${error}`);
-    return '';
+import { findPerson, sortArr } from './data.js';
+
+const mainContainer = document.querySelector('main');
+const sidebarContainer = document.querySelector('.content');
+
+const result = await sortArr().then((data) => data);
+
+const createItem = function (container, image, alt, text) {
+  const actualImg = container.querySelector('img');
+  const actualText = container.querySelector('p');
+  actualImg.src = image;
+  actualImg.alt = alt;
+  actualText.textContent = text;
+};
+
+const createOneItem = function () {
+  const item = document.createElement('div');
+  item.classList.add('row__item');
+  const img = document.createElement('img');
+  const par = document.createElement('p');
+  item.appendChild(img);
+  item.append(par);
+
+  mainContainer.appendChild(item);
+};
+
+const createAllItems = function () {
+  for (let i = 0; i < result.length; i += 1) {
+    createOneItem();
   }
 };
 
-getData();
-
-const filterData = async () => {
-  try {
-    const arr = await getData().then((data) => data);
-    const filteredArr = arr.filter((item) => item.dead !== true).slice(0, -1);
-    return filteredArr;
-  } catch (error) {
-    console.error(`Valami hiba van az adatok szűrésekor: ${error}`);
-    return '';
-  }
+const loadData = () => {
+  createAllItems();
+  const allItem = Array.from(document.querySelectorAll('.row__item'));
+  allItem.forEach((element, index) => {
+    const actualPerson = result[index];
+    createItem(element, actualPerson.portrait, actualPerson.name, actualPerson.name);
+  });
 };
 
-const sortArr = async () => {
-  try {
-    const arr = await filterData().then((data) => data);
-    const sortedArr = arr.sort((a, b) => a.name.localeCompare(b.name, 'hu'));
+loadData();
 
-    return sortedArr;
-  } catch (error) {
-    console.error(`Valami hiba van az adatok rendezésekor: ${error}`);
-    return '';
-  }
+const madeSideBarContent = () => {
+  const image = document.createElement('img');
+  image.classList.add('sidebarPicture');
+  sidebarContainer.appendChild(image);
+  const nameWithLogo = document.createElement('div');
+  nameWithLogo.classList.add('nameWithLogo');
+  const personName = document.createElement('h1');
+  const logo = document.createElement('img');
+  logo.classList.add('logo');
+  nameWithLogo.appendChild(personName);
+  nameWithLogo.appendChild(logo);
+  sidebarContainer.appendChild(nameWithLogo);
+  const text = document.createElement('p');
+  sidebarContainer.appendChild(text);
+
+  return [image, personName, logo, text];
 };
 
-sortArr().then(console.log);
+madeSideBarContent();
+
+const modifySidebar = (image, name, logo, text) => {
+  const [imageBig, personName, houselogo, details] = madeSideBarContent();
+
+  imageBig.src = image;
+  personName.textContent = name;
+  houselogo.src = logo;
+  details.textContent = text;
+
+  /* const actualImg = sidebarContainer.querySelector('.sidebarPicture');
+  const actualName = sidebarContainer.querySelector('h1')
+  const actualLogo = sidebarContainer.querySelector('.logo')
+  const actualText = sidebarContainer.querySelector() */
+};
+
+const loadSideBar = async (person) => {
+  const actualPerson = await findPerson(person);
+  console.log(actualPerson);
+  const logoOfPerson = `./assets/houses/${actualPerson.house ? actualPerson.house : 'nothing'}.png`;
+  modifySidebar(actualPerson.picture, actualPerson.name, logoOfPerson, actualPerson.bio);
+};
+
+
+loadSideBar('viserys targaryen');
